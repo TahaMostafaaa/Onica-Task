03ed16156614502f893efe8535bcbb67fd322d2d
Taha Goda <taha.goda.ext@orange.com>
2020-02-21 18:08:21 +0200

    Add Real Data and classify

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,20 +8,26 @@ import java.util.Scanner;
 
 public class Main {
 
-     public static void startUI(){
-         System.out.println("==== Book Manager ====\n 1) View all books\n 2) Add a book\n 3) Edit a book\n 4) Search for a book\n 5) Save and exit\n Choose [1-5]: ");
+    static Scanner scanner = new Scanner(System.in);
 
+     public static void startUI() throws FileNotFoundException {
+         System.out.println("==== Book Manager ====\n 1) View all books\n 2) Add a book\n 3) Edit a book\n 4) Search for a book\n 5) Save and exit\n Choose [1-5]: ");
+         if(scanner.nextInt() == 1){
+             viewAllBooks();
+         }
      }
      public static void viewAllBooks() throws FileNotFoundException {
 
 //         URL main = Main.class.getResource("Main.class");
 //         System.out.println(main.getPath());
          File file = new File("C:/Users/vdcg9145/Desktop/Play Ground/Onica-Task/out/production/Onica-Task/com/company/data.txt");
-
+         System.out.println("==== View Books ====");
          Scanner sc = new Scanner(file);
          while (sc.hasNextLine())
              {
-                 System.out.println(sc.nextLine());
+                 String bookData[] = sc.nextLine().split(",");
+                 System.out.println("["+bookData[0]+"] "+bookData[1]);
+
              }
      }
 
