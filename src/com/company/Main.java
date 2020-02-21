4eb475faf0a01b462c7c065f27c28515d03d9ea5
Taha Goda <taha.goda.ext@orange.com>
2020-02-21 21:03:04 +0200

    Finialize flow and all scenarios

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,12 +20,9 @@ public class Main {
         }
     }
 
-
     public static void startUI() throws FileNotFoundException {
-         System.out.println("==== Book Manager ====\n 1) View all books\n 2) Add a book\n 3) Edit a book\n 4) Search for a book\n 5) Save and exit\n Choose [1-5]: ");
-         if(inputScanner.nextInt() == 1){
-             viewAllBooks();
-         }
+         System.out.print("==== Book Manager ====\n 1) View all books\n 2) Add a book\n 3) Edit a book\n 4) Search for a book\n 5) Save and exit\n Choose [1-5]: ");
+
      }
     public static void addBook() throws IOException {
         System.out.print("==== Add a Book ====\nPlease enter the following information:\n Title: ");
@@ -42,13 +39,15 @@ public class Main {
         bufferedWriter.append("\n" +(count+1)+ "," + title +"," + author + "," + description);
         }finally {
             bufferedWriter.close();
-            System.out.println("Saved");
+            System.out.format("Book [%s] Saved",count+1);
         }
     }
 
     public static void editBook() throws IOException {
+
         System.out.println("==== Edit a Book ====");
         List<String> arrayList =  new ArrayList<String>();
+        inputScanner.nextLine();
         while (fileScanner.hasNextLine())
         {
             String book = fileScanner.nextLine();
@@ -57,11 +56,13 @@ public class Main {
             System.out.println(" [" + bookData[0] + "] " + bookData[1]);
 
         }
-        System.out.print("\n Book ID: ");
-        int id = inputScanner.nextInt();
-        String editableBook[] = arrayList.get(id-1).split(",");
+        scopeOfRepeating: while(true){ System.out.print("Enter the book ID of the book you want to edit; to return press <Enter>.\n");
+        System.out.print(" Book ID: ");
+        String id = inputScanner.nextLine();
+        if(id.equals("")) break scopeOfRepeating;
+        String editableBook[] = arrayList.get(Integer.parseInt(id)-1).split(",");
         System.out.format("Input the following information. To leave a field unchanged, hit <Enter> \n Title [%s]: ",editableBook[1]);
-        inputScanner.nextLine();
+
         String title = inputScanner.nextLine();
         if(title.equals("")) title = editableBook[1];
         System.out.format(" Author [%s]: ", editableBook[2]);
@@ -71,8 +72,8 @@ public class Main {
         String description = inputScanner.nextLine();
         if(description.equals("")) description = editableBook[3];
 
-        String newBookAfterUpdate = String.format("%d,%s,%s,%s",id,title,author,description);
-        arrayList.set(id-1,newBookAfterUpdate);
+        String newBookAfterUpdate = String.format("%s,%s,%s,%s",id,title,author,description);
+        arrayList.set(Integer.parseInt(id)-1,newBookAfterUpdate);
 
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file.getPath()));
         try {
@@ -80,12 +81,20 @@ public class Main {
                     bufferedWriter.write(arrayList.get(i)+"\n");
         }finally {
             bufferedWriter.close();
-            System.out.println("Saved");
+            System.out.println("Book Edited & Saved");
+
         }
+}
+
+
+
+
 
     }
-    public static void searchForBook() throws FileNotFoundException {
+    public static void searchForBook(boolean firstTime) throws FileNotFoundException {
            System.out.print("To view details enter the book ID, to return press <Enter>.\n Book ID: ");
+           if(firstTime)
+           inputScanner.nextLine();
            String id = inputScanner.nextLine();
            if(id.equals("")) return;
             fileScanner = new Scanner(file);
@@ -100,24 +109,47 @@ public class Main {
 
         }
 
-       searchForBook();
+       searchForBook(false);
 
     }
+
+
      public static void viewAllBooks() throws FileNotFoundException {
 
 //         URL main = Main.class.getResource("Main.class");
 //         System.out.println(main.getPath());
          System.out.println("==== View Books ====");
+         fileScanner = new Scanner(file);
          while (fileScanner.hasNextLine())
              {
                  String bookData[] = fileScanner.nextLine().split(",");
                  System.out.println("[" + bookData[0] + "] " + bookData[1]);
 
              }
+
      }
 
     public static void main(String[] args) throws IOException {
-        searchForBook();
+        int operation;
+
+        while (true){
+            startUI();
+            operation = inputScanner.nextInt();
+            if(operation == 1){
+                viewAllBooks();
+                searchForBook(true);
+            }else if(operation == 2){
+                addBook();
+            }else if (operation == 3){
+                editBook();
+            }else if( operation == 4){
+                searchForBook(true);
+            }else
+            {
+                System.out.println("Library saved."); // we already saving in every step
+                return;
+            }
+        }
 
     }
 }
