59746b6481124cf289687fd6ec856ffd2bf9888e
Taha Goda <taha.goda.ext@orange.com>
2020-02-21 17:35:40 +0200

    Add first UI Lines

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+     public static void startUI(){
+         System.out.println("==== Book Manager ====\n 1) View all books\n 2) Add a book\n 3) Edit a book\n 4) Search for a book\n 5) Save and exit\n Choose [1-5]: ");
+     }
+
+    public static void main(String[] args) {
+	startUI();
+
+    }
+}
